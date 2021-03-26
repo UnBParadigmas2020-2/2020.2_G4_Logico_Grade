6f10c070ce7441d7b0034337720b3d21288fb463
Marcos <marcosnery.comp@gmail.com>
2021-03-26 05:41:52 -0300

    Adding function to get semester as input, and also an exit function

--- setup.pl
+++ setup.pl
@@ -38,3 +38,19 @@ print_list_of_lists([Element|List]) :-
 get_disciplines_by_semester(Semester) :-
     findall([Code, Name, Hours, Status], 
             discipline(Code, Name, Hours, Status, Semester), List), print_list_of_lists(List).
+
+
+/*Print all disciplines of a given semester*/
+print_disciplines_by_semester :-
+    write_ln('Digite o numero do semestre para o qual deseja listar disciplinas, seguido de um ponto final'), nl,
+    read(Semester_Integer),
+    /*Converting integer to string*/
+    number_string(Semester_Integer, Semester),
+    write('Essas são as disciplinas do semestre '), write(Semester), write_ln(": "),
+    get_disciplines_by_semester(Semester).
+
+
+/*Exit program*/
+exit():-
+    write_ln('Até logo !'),
+    halt(0).
